e72a69a00fb9ceb31d54933e18b31010ff0da535
fnathasya <faizaanathasya@gmail.com>
2023-11-12 15:49:11 +0700

    first commit

---
+++ MyFirstJava13.java
@@ -0,0 +1,7 @@
+public class MyFirstJava13 {
+    public static void main(String[] args) {
+
+        System.out.println("Nama saya Faiza Anathasya Eka Falen");
+        System.out.println("saya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+    }
+}
---
+++ Tugas1.java
@@ -0,0 +1,10 @@
+public class Tugas1 {
+    public static void main(String[] args) {
+
+    System.out.println("Nama : Faiza Anathasya Eka Falen");
+    System.out.println("Absen : 13");
+    System.out.println("Kelas : 1A");
+    System.out.println("\nProdi : D-IV Sistem Informasi Bisnis");
+    System.out.println("Jurusan : Teknologi Informasi");
+    }
+}
